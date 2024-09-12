38d020e2f45d49f2a48e9ce84820354408403a0f
Salaev78 <t.salaev@outlook.com>
2024-09-12 14:10:49 +0200

    back to top

--- scripts.js
+++ scripts.js
@@ -7,3 +7,19 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+ window.addEventListener('scroll', function() {
+     const button = document.getElementById('backToTop');
+     if (window.scrollY > 300) {
+         button.style.display = 'block';
+     } else {
+         button.style.display = 'none';
+     }
+ });
+ 
+ document.getElementById('backToTop').addEventListener('click', function() {
+     window.scrollTo({
+         top: 0,
+         behavior: 'smooth'
+     });
+ });
