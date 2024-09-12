af38517eb67aa428b362aed54704ed125b21db9d
Salaev78 <t.salaev@outlook.com>
2024-09-12 14:37:03 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,18 +8,18 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
- window.addEventListener('scroll', function() {
-     const button = document.getElementById('backToTop');
-     if (window.scrollY > 300) {
-         button.style.display = 'block';
-     } else {
-         button.style.display = 'none';
-     }
- });
- 
- document.getElementById('backToTop').addEventListener('click', function() {
-     window.scrollTo({
-         top: 0,
-         behavior: 'smooth'
-     });
- });
+window.addEventListener('scroll', function() {
+    const button = document.getElementById('backToTop');
+    if (window.scrollY > 300) {
+        button.style.display = 'block'; // Show the button when scrolled down
+    } else {
+        button.style.display = 'none'; // Hide the button when scrolled up
+    }
+});
+
+document.getElementById('backToTop').addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
